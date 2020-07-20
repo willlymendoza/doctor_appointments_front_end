9e92a82af2519de1fe9b93a646e46ee01cdbf49c
Willian <wyllmh5@gmail.com>
2020-07-20 15:25:44 -0600

    Added Appointments, Dashboard, Patients & Users pages

---
+++ src/pages/Appointments/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Appointments = () => {
+  return (
+    <Fragment>
+      <h1>Appointments</h1>
+    </Fragment>
+  );
+};
+
+export default Appointments;
