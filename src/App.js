f38839d6b20fdc71655f52874965a09f50eb0bfb
Willian <wyllmh5@gmail.com>
2020-07-20 15:24:26 -0600

    Implemented Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 
 import Header from "./components/Header";
-import Main from "./components/Main";
 import Footer from "./components/Footer";
+
+import Dashboard from "./pages/Dashboard";
+import Appointments from "./pages/Appointments";
+import Patients from "./pages/Patients";
+import Users from "./pages/Users";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <Main />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Dashboard}></Route>
+          <Route path="/appointments" exact component={Appointments}></Route>
+          <Route path="/patients" exact component={Patients}></Route>
+          <Route path="/users" exact component={Users}></Route>
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
