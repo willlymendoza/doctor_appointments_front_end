73b1f38d85d0d7c1f32666d088e00f16f73bcf0b
Willian <wyllmh5@gmail.com>
2020-07-20 20:13:36 -0600

    Implemented Router

--- src/App.js
+++ src/App.js
@@ -8,21 +8,34 @@ import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
 import Appointments from "./pages/Appointments";
 import Patients from "./pages/Patients";
+import AddPatient from "./pages/Patients/AddPatient";
 import Users from "./pages/Users";
+import Main from "./components/Main";
+import AddAppointment from "./pages/Appointments/AddAppointment";
 
 function App() {
   return (
     <div className="container">
-      <Header />
       <Router>
-        <Switch>
-          <Route path="/" exact component={Dashboard}></Route>
-          <Route path="/appointments" exact component={Appointments}></Route>
-          <Route path="/patients" exact component={Patients}></Route>
-          <Route path="/users" exact component={Users}></Route>
-        </Switch>
+        <Header />
+
+        <Main>
+          <Switch>
+            <Route path="/" exact component={Dashboard}></Route>
+            <Route path="/appointments" exact component={Appointments}></Route>
+            <Route
+              path="/appointments/add"
+              exact
+              component={AddAppointment}
+            ></Route>
+            <Route path="/patients" exact component={Patients}></Route>
+            <Route path="/patients/add" exact component={AddPatient}></Route>
+            <Route path="/users" exact component={Users}></Route>
+          </Switch>
+        </Main>
+
+        <Footer />
       </Router>
-      <Footer />
     </div>
   );
 }
