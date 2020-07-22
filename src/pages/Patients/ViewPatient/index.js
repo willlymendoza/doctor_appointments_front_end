86a70989e0de37e19f5a11824801395b190918bf
Willian <wyllmh5@gmail.com>
2020-07-21 21:34:00 -0600

    Added View Patient page

---
+++ src/pages/Patients/ViewPatient/index.js
@@ -0,0 +1,80 @@
+import React, { Fragment } from "react";
+import { useParams } from "react-router-dom";
+import PageTitle from "../../../components/PageTitle";
+import PatientForm from "../../../components/PatientForm";
+
+import "./styles.scss";
+import CardContainer from "../../../components/CardContainer";
+
+const ViewPatient = () => {
+  const { id } = useParams();
+
+  return (
+    <Fragment>
+      <PageTitle title={`PATIENT ${id}`} />
+      <div className="patient-info-container">
+        <PatientForm buttonText="edit" />
+        <CardContainer title="Appointments" color="warning_color">
+          <div className="list-container">
+            <div>
+              <span>2020-07-18</span>
+              <span>15:30</span>
+              <span>
+                <a href="/">
+                  <i className="fas fa-eye"></i>
+                </a>
+              </span>
+            </div>
+            <div>
+              <span>2020-07-18</span>
+              <span>15:30</span>
+              <span>
+                <a href="/">
+                  <i className="fas fa-eye"></i>
+                </a>
+              </span>
+            </div>
+            <div>
+              <span>2020-07-18</span>
+              <span>15:30</span>
+              <span>
+                <a href="/">
+                  <i className="fas fa-eye"></i>
+                </a>
+              </span>
+            </div>
+            <div>
+              <span>2020-07-18</span>
+              <span>15:30</span>
+              <span>
+                <a href="/">
+                  <i className="fas fa-eye"></i>
+                </a>
+              </span>
+            </div>
+            <div>
+              <span>2020-07-18</span>
+              <span>15:30</span>
+              <span>
+                <a href="/">
+                  <i className="fas fa-eye"></i>
+                </a>
+              </span>
+            </div>
+            <div>
+              <span>2020-07-18</span>
+              <span>15:30</span>
+              <span>
+                <a href="/">
+                  <i className="fas fa-eye"></i>
+                </a>
+              </span>
+            </div>
+          </div>
+        </CardContainer>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ViewPatient;
