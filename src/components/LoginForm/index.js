226fbd47ef3a00d08d5d4e94a18e8293c4736560
Willian <wyllmh5@gmail.com>
2020-07-23 01:00:16 -0600

    Added handleSubmit function

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,13 +1,30 @@
 import React, { Fragment } from "react";
 import CardContainer from "../CardContainer";
+import useAxios from "../../hooks/useAxios";
+import axios from "axios";
+
 import "./styles.scss";
 
 const LoginForm = () => {
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    /* getPatients(); */
+    console.log("submited");
+  };
+
+  /* const getPatients = async () => {
+    const rest = await axios.get("http://localhost:5000/api/patients");
+    console.log(rest.data);
+  }; */
+
+  const result = useAxios("http://localhost:5000/api/patients");
+  console.log(result);
+
   return (
     <Fragment>
       <div className="form-container login-container">
         <CardContainer title="Account Login" color="secondary_color">
-          <form className="form">
+          <form className="form" onSubmit={handleOnSubmit}>
             <div className="form-group full-width">
               <label>E-mail</label>
               <input type="text" name="email" />
