63371e88e946f62bd7e1d9f639666835a4352193
Willian <wyllmh5@gmail.com>
2020-07-23 18:35:14 -0600

    Renamed loginDuck to AuthDuck

---
+++ src/redux/authDuck.js
@@ -0,0 +1,50 @@
+// constants
+
+const initialData = {
+  userInfo: JSON.parse(localStorage.getItem("userInfo")) || [],
+  userToken: localStorage.getItem("userToken") || "",
+  isAuthenticated: localStorage.getItem("isAuthenticated") || false,
+};
+
+// types
+
+const SET_LOGIN = "SET_LOGIN";
+const SET_LOGOUT = "SET_LOGOUT";
+
+// reducer
+
+export default function loginReducer(state = initialData, action) {
+  switch (action.type) {
+    case SET_LOGIN:
+      return {
+        ...state,
+        userInfo: action.payload.user,
+        userToken: action.payload.userToken,
+        isAuthenticated: true,
+      };
+    case SET_LOGOUT:
+      return {
+        ...state,
+        userInfo: [],
+        userToken: "",
+        isAuthenticated: false,
+      };
+    default:
+      return state;
+  }
+}
+
+// actions
+
+export const setLoginAction = (data) => async (dispatch, getState) => {
+  dispatch({
+    type: SET_LOGIN,
+    payload: data,
+  });
+};
+
+export const setLogoutAction = () => async (dispatch, getState) => {
+  dispatch({
+    type: SET_LOGOUT,
+  });
+};
