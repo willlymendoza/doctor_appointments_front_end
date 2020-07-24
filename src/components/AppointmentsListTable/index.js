0b83aa32ff52136e86034fab9a0db72fe6c3d7cc
Willian <wyllmh5@gmail.com>
2020-07-24 15:46:05 -0600

    Showing data from back-end

--- src/components/AppointmentsListTable/index.js
+++ src/components/AppointmentsListTable/index.js
@@ -1,11 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import CardContainer from "../../components/CardContainer";
 import CustomPagination from "../../components/CustomPagination";
+import axios from "axios";
 
 import "./styles.scss";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const AppointmentsListTable = () => {
+  const [appointmentsData, setAppointmentsData] = useState([]);
+
+  const userToken = useSelector((store) => store.authData.userToken);
+
+  useEffect(() => {
+    const getAppointments = async () => {
+      try {
+        const getListAppointments = await axios.get(
+          "http://localhost:5000/api/appointments",
+          {
+            headers: {
+              Authorization: userToken,
+            },
+          }
+        );
+        const result = getListAppointments.data;
+        console.log(result);
+        setAppointmentsData(result);
+      } catch (error) {
+        if (error.response) console.log(error.response.data);
+      }
+    };
+
+    getAppointments();
+  }, [userToken]);
+
   return (
     <Fragment>
       <NavLink to="/appointments/add">
@@ -19,172 +47,33 @@ const AppointmentsListTable = () => {
       </NavLink>
       <CardContainer title="List Of Appointments" color="warning_color">
         <div className="table-grid table-header">
-          <label>First Name</label>
-          <label>Last Name</label>
-          <label>Personal ID</label>
-          <label>E-mail</label>
+          <label>Date</label>
+          <label>Hour</label>
+          <label>Patient</label>
+          <label>Doctor</label>
           <label>Actions</label>
         </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
-        <div className="table-grid table-content">
-          <span>
-            <label>Name: </label>Willian Wosbely
-          </span>
-          <span>Mendoza Hernandez</span>
-          <span>09:30</span>
-          <span>Dr. Jhon Doe</span>
-          <span className="actions">
-            <a href="/" className="edit_item">
-              <i className="fas fa-edit"></i>
-            </a>
-            <a href="/" className="view_item">
-              <i className="fas fa-eye"></i>
-            </a>
-          </span>
-        </div>
+        {appointmentsData.map((appointment) => (
+          <div key={appointment._id} className="table-grid table-content">
+            <span>{appointment.appointment_date}</span>
+            <span>{appointment.hour}</span>
+            <span>
+              <label>Name: </label>
+              {appointment.patient.first_name} {appointment.patient.last_name}
+            </span>
+            <span>
+              {appointment.doctor.name} {appointment.doctor.last_name}
+            </span>
+            <span className="actions">
+              <a href="/" className="edit_item">
+                <i className="fas fa-edit"></i>
+              </a>
+              <a href="/" className="view_item">
+                <i className="fas fa-eye"></i>
+              </a>
+            </span>
+          </div>
+        ))}
       </CardContainer>
       <CustomPagination
         color="warning"
