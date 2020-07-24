f3c34da04ba91858042ee3dc234f1c37b2db14b6
Willian <wyllmh5@gmail.com>
2020-07-24 12:59:00 -0600

    Added validation for the navbar when user is Authenticated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,21 @@
 import React, { Fragment } from "react";
 import NavBar from "../NavBar";
 import Logo from "../Logo";
+import { useSelector } from "react-redux";
+
 import "./styles.scss";
 
 const Header = () => {
+  const isAuthenticated = useSelector(
+    (store) => store.authData.isAuthenticated
+  );
+
   return (
     <Fragment>
       <div className="header-container"></div>
       <header className="header-content">
         <Logo />
-        <NavBar />
+        {isAuthenticated ? <NavBar /> : ""}
       </header>
     </Fragment>
   );
