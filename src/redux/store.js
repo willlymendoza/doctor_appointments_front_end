c3d3f0ce74ee813895156b0db7cb1a283109b93f
Willian <wyllmh5@gmail.com>
2020-07-23 21:32:02 -0600

    Used redux-persist with authData reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,26 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import authReducer from "./authDuck";
 
+const persistConfig = {
+  key: "root",
+  storage: storage,
+};
+
 const rootReducer = combineReducers({
-  authData: authReducer,
+  authData: persistReducer(persistConfig, authReducer),
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function generateState() {
-  const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
-  );
-  return store;
-}
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const persistor = persistStore(store);
+
+export { persistor, store };
