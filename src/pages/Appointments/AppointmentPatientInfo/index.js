d1623d12e3dd902cc7e667db25b9efa62e2dd556
Willian <wyllmh5@gmail.com>
2020-07-28 16:38:38 -0600

    Added AppointmentPatietnInfo component

---
+++ src/pages/Appointments/AppointmentPatientInfo/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import CardContainer from "../../../components/CardContainer";
+import "./styles.scss";
+
+const AppointmentPatientInfo = ({ patientInfo }) => {
+  return (
+    <CardContainer title="Patient Info" color="secondary_color">
+      <div className="patient-info">
+        <div>
+          <label>Name: </label>
+          <span>
+            {patientInfo.first_name} {patientInfo.last_name}
+          </span>
+        </div>
+        <div>
+          <label>Personal Document ID: </label>
+          <span>{patientInfo.personal_document_id}</span>
+        </div>
+        <div>
+          <label>E-mail: </label>
+          <span>{patientInfo.email}</span>
+        </div>
+        <div>
+          <label>Phone Number: </label>
+          <span>{patientInfo.phone_number}</span>
+        </div>
+        <div>
+          <label>City: </label>
+          <span>{patientInfo.city}</span>
+        </div>
+        <div>
+          <label>Address: </label>
+          <span>{patientInfo.address}</span>
+        </div>
+        <div>
+          <label>Age: </label>
+          <span>{patientInfo.age}</span>
+        </div>
+        <div>
+          <label>Sex: </label>
+          <span>{patientInfo.sex}</span>
+        </div>
+      </div>
+    </CardContainer>
+  );
+};
+
+export default AppointmentPatientInfo;
