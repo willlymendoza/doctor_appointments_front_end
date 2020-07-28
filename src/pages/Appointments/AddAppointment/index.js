260a348b55087c6b125ed04613b937fc2b81c13f
Willian <wyllmh5@gmail.com>
2020-07-28 15:57:04 -0600

    Moved functions from AppintmentAddForm

--- src/pages/Appointments/AddAppointment/index.js
+++ src/pages/Appointments/AddAppointment/index.js
@@ -1,12 +1,107 @@
-import React, { Fragment } from "react";
-import AppointmentForm from "../../../components/AppointmentForm";
+import React, { Fragment, useState, useEffect } from "react";
+import AppointmentAddForm from "../../../components/AppointmentAddForm";
 import PageTitle from "../../../components/PageTitle";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const AddAppointment = () => {
+  const formData = {
+    patient_id: "",
+    doctor_id: "",
+    observations: "",
+    appointment_date: "",
+    hour: "",
+  };
+
+  const [appointmentInfo, setAppointmentInfo] = useState(formData);
+  const [doctorsList, setDoctorsList] = useState([]);
+  const [patientsList, setPatientsList] = useState([]);
+  const { userToken } = useSelector((store) => store.authData);
+  const history = useHistory();
+
+  const handleInputChange = (e) => {
+    setAppointmentInfo({
+      ...appointmentInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSelectChange = (newValue, actionMeta) => {
+    setAppointmentInfo({
+      ...appointmentInfo,
+      [actionMeta.name]: newValue._id,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    axiosPostData();
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    const getDoctors = async () => {
+      try {
+        const result = await axios.get("http://localhost:5000/api/users", {
+          headers: {
+            Authorization: userToken,
+          },
+        });
+
+        setDoctorsList(result.data.filter((item) => item.is_doctor === true));
+      } catch (error) {
+        if (error.response) console.log(error.response.data);
+      }
+    };
+    getDoctors();
+  }, [userToken]);
+
+  useEffect(() => {
+    const getPatients = async () => {
+      try {
+        const result = await axios.get("http://localhost:5000/api/patients", {
+          headers: {
+            Authorization: userToken,
+          },
+        });
+
+        setPatientsList(result.data);
+      } catch (error) {
+        if (error.response) console.log(error.response.data);
+      }
+    };
+    getPatients();
+  }, [userToken]);
+
+  const axiosPostData = async () => {
+    try {
+      await axios.post(
+        "http://localhost:5000/api/appointments",
+        appointmentInfo,
+        {
+          headers: {
+            Authorization: userToken,
+          },
+        }
+      );
+      history.push("/appointments");
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+      }
+    }
+  };
+
   return (
     <Fragment>
       <PageTitle title="ADD APPOINTMENT" />
-      <AppointmentForm />
+      <AppointmentAddForm
+        handleOnSubmit={handleOnSubmit}
+        handleInputChange={handleInputChange}
+        handleSelectChange={handleSelectChange}
+        patientsList={patientsList}
+        doctorsList={doctorsList}
+      />
     </Fragment>
   );
 };
