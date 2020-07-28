e7f20c105c1b53fb214ce8b356c1bda30d22392e
Willian <wyllmh5@gmail.com>
2020-07-28 14:27:18 -0600

    Moved functions to ViewAppointment component and added params

--- src/components/AppointmentViewForm/index.js
+++ src/components/AppointmentViewForm/index.js
@@ -1,127 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import CardContainer from "../../components/CardContainer";
 import Select from "react-select";
-import axios from "axios";
 import moment from "moment";
-import { useParams } from "react-router-dom";
 
-const AppointmentViewForm = () => {
-  const { id } = useParams();
-  const [appointmentInfo, setAppointmentInfo] = useState(null);
-  const [patientInfo, setPatientInfo] = useState([]);
-  const [patientsList, setPatientsList] = useState([]);
-  const [doctorsList, setDoctorsList] = useState([]);
-  const [disabledInput, setDisabledInput] = useState(true);
-  const userToken = useSelector((store) => store.authData.userToken);
-
-  const handleInputChange = (e) => {
-    setAppointmentInfo({
-      ...appointmentInfo,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSelectChange = (newValue, actionMeta) => {
-    setAppointmentInfo({
-      ...appointmentInfo,
-      [actionMeta.name]: newValue._id,
-    });
-  };
-
-  const handleEditClick = (e) => {
-    e.preventDefault();
-    setDisabledInput(false);
-  };
-
-  const handleOnSubmit = (e) => {
-    axiosPostData();
-    e.preventDefault();
-  };
-
-  const axiosPostData = async () => {
-    try {
-      const { _id, id, ...postData } = appointmentInfo;
-      await axios.put(
-        `http://localhost:5000/api/appointments/${id}`,
-        postData,
-        {
-          headers: {
-            Authorization: userToken,
-          },
-        }
-      );
-      setDisabledInput(true);
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.data);
-      }
-    }
-  };
-
-  /* getting list of doctors */
-  useEffect(() => {
-    const getDoctors = async () => {
-      try {
-        const result = await axios.get("http://localhost:5000/api/users", {
-          headers: {
-            Authorization: userToken,
-          },
-        });
-
-        setDoctorsList(result.data.filter((item) => item.is_doctor === true));
-      } catch (error) {
-        if (error.response) console.log(error.response.data);
-      }
-    };
-    getDoctors();
-  }, [userToken]);
-
-  /* getting list of patients */
-  useEffect(() => {
-    const getPatients = async () => {
-      try {
-        const result = await axios.get("http://localhost:5000/api/patients", {
-          headers: {
-            Authorization: userToken,
-          },
-        });
-
-        setPatientsList(result.data);
-      } catch (error) {
-        if (error.response) console.log(error.response.data);
-      }
-    };
-    getPatients();
-  }, [userToken]);
-
-  /* getting appointment info */
-  useEffect(() => {
-    const getAppointmentInfo = async () => {
-      try {
-        const result = await axios.get(
-          `http://localhost:5000/api/appointments/${id}`,
-          {
-            headers: {
-              Authorization: userToken,
-            },
-          }
-        );
-
-        setPatientInfo(result.data.patient);
-
-        delete result.data.doctor;
-        delete result.data.patient;
-
-        setAppointmentInfo(result.data);
-      } catch (error) {
-        if (error.response) console.log(error.response.data);
-      }
-    };
-
-    getAppointmentInfo();
-  }, [id, userToken]);
-
+const AppointmentViewForm = ({
+  patientsList,
+  doctorsList,
+  appointmentInfo,
+  handleOnSubmit,
+  disabledInput,
+  handleSelectChange,
+  handleInputChange,
+  handleEditClick,
+}) => {
   const customSelectStyles = {
     valueContainer: () => ({
       height: 37,
