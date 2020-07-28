52188863deabe8020c18358875ab8bbfaacf81d7
Willian <wyllmh5@gmail.com>
2020-07-28 12:51:02 -0600

    Moved function from PatientViewForm and show list of appointments

--- src/pages/Patients/ViewPatient/index.js
+++ src/pages/Patients/ViewPatient/index.js
@@ -1,75 +1,119 @@
-import React, { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import React, { Fragment, useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import PageTitle from "../../../components/PageTitle";
 import PatientViewForm from "../../../components/PatientViewForm";
 
 import "./styles.scss";
 import CardContainer from "../../../components/CardContainer";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 const ViewPatient = () => {
   const { id } = useParams();
+  const [patientInfo, setPatientInfo] = useState([]);
+  const [appointmentsInfo, setAppointmentsInfo] = useState([]);
+  const [disabledInput, setDisabledInput] = useState(true);
+  const userToken = useSelector((store) => store.authData.userToken);
+
+  const handleInputChange = (e) => {
+    setPatientInfo({
+      ...patientInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleEditClick = (e) => {
+    e.preventDefault();
+    setDisabledInput(false);
+  };
+
+  const handleOnSubmit = (e) => {
+    axiosPostData();
+    e.preventDefault();
+  };
+
+  const axiosPostData = async () => {
+    try {
+      const { _id, ...postData } = patientInfo;
+      await axios.put(`http://localhost:5000/api/patients/${id}`, postData, {
+        headers: {
+          Authorization: userToken,
+        },
+      });
+      setDisabledInput(true);
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const getPatientInfo = async () => {
+      try {
+        const result = await axios.get(
+          `http://localhost:5000/api/patients/${id}`,
+          {
+            headers: {
+              Authorization: userToken,
+            },
+          }
+        );
+        setPatientInfo(result.data);
+      } catch (error) {
+        if (error.response) console.log(error.response.data);
+      }
+    };
+
+    getPatientInfo();
+  }, [id, userToken]);
+
+  useEffect(() => {
+    const getPatientAppointments = async () => {
+      try {
+        const result = await axios.get(
+          `http://localhost:5000/api/appointments/patient/${id}`,
+          {
+            headers: {
+              Authorization: userToken,
+            },
+          }
+        );
+        setAppointmentsInfo(result.data);
+      } catch (error) {
+        if (error.response) console.log(error.response.data);
+      }
+    };
+
+    getPatientAppointments();
+  }, [id, userToken]);
 
   return (
     <Fragment>
       <PageTitle title={`PATIENT ${id}`} />
       <div className="patient-info-container">
-        <PatientViewForm />
+        <PatientViewForm
+          patientInfo={patientInfo}
+          handleOnSubmit={handleOnSubmit}
+          disabledInput={disabledInput}
+          handleInputChange={handleInputChange}
+          handleEditClick={handleEditClick}
+        />
+
+        {/* PATIENT INFO CONTAINER */}
         <CardContainer title="Appointments" color="warning_color">
           <div className="list-container">
-            <div>
-              <span>2020-07-18</span>
-              <span>15:30</span>
-              <span>
-                <a href="/">
-                  <i className="fas fa-eye"></i>
-                </a>
-              </span>
-            </div>
-            <div>
-              <span>2020-07-18</span>
-              <span>15:30</span>
-              <span>
-                <a href="/">
-                  <i className="fas fa-eye"></i>
-                </a>
-              </span>
-            </div>
-            <div>
-              <span>2020-07-18</span>
-              <span>15:30</span>
-              <span>
-                <a href="/">
-                  <i className="fas fa-eye"></i>
-                </a>
-              </span>
-            </div>
-            <div>
-              <span>2020-07-18</span>
-              <span>15:30</span>
-              <span>
-                <a href="/">
-                  <i className="fas fa-eye"></i>
-                </a>
-              </span>
-            </div>
-            <div>
-              <span>2020-07-18</span>
-              <span>15:30</span>
-              <span>
-                <a href="/">
-                  <i className="fas fa-eye"></i>
-                </a>
-              </span>
-            </div>
-            <div>
-              <span>2020-07-18</span>
-              <span>15:30</span>
-              <span>
-                <a href="/">
-                  <i className="fas fa-eye"></i>
-                </a>
-              </span>
-            </div>
+            {appointmentsInfo.map((item) => (
+              <div key={item._id}>
+                <span>{item.appointment_date}</span>
+                <span>{item.hour}</span>
+                <span>
+                  <Link to={`/appointments/${item._id}`}>
+                    <i className="fas fa-eye"></i>
+                  </Link>
+                </span>
+              </div>
+            ))}
           </div>
         </CardContainer>
       </div>
