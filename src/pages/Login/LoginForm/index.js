cad6f6d040d2d4ea6b31550ea605657338d9dabb
Willian <wyllmh5@gmail.com>
2020-07-28 22:30:31 -0600

    Moved functions to Login component

--- src/pages/Login/LoginForm/index.js
+++ src/pages/Login/LoginForm/index.js
@@ -1,77 +1,52 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import CardContainer from "../../../components/CardContainer";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { setLoginAction } from "../../../redux/authDuck";
 
 import "./styles.scss";
 
-const LoginForm = (props) => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const dispatch = useDispatch();
-  const history = useHistory();
-
-  const handleInputChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    axiosPostData(formData);
-  };
-
-  const axiosPostData = async (data) => {
-    try {
-      const result = await axios.post("http://localhost:5000/api/auth", {
-        email: data.email,
-        password: data.password,
-      });
-
-      dispatch(
-        setLoginAction({
-          user: result.data,
-          userToken: result.headers.authorization,
-        })
-      );
-
-      history.push("/");
-    } catch (error) {
-      if (error.response) {
-        console.log(error.response.data);
-      }
-    }
-  };
-
+const LoginForm = ({
+  handleSubmit,
+  onSubmitForm,
+  register,
+  formValidations,
+  errors,
+  requestError,
+}) => {
   return (
     <Fragment>
       <div className="form-container login-container">
         <CardContainer title="Account Login" color="secondary_color">
-          <form className="form" onSubmit={handleOnSubmit}>
+          <form className="form" onSubmit={handleSubmit(onSubmitForm)}>
             <div className="form-group full-width">
               <label>E-mail</label>
               <input
                 type="text"
                 name="email"
                 autoFocus
-                onChange={handleInputChange}
+                ref={register(formValidations.email)}
               />
+              <span className="form-error-message">
+                {errors?.email?.message}
+              </span>
             </div>
             <div className="form-group full-width">
               <label>Password</label>
               <input
                 type="password"
                 name="password"
-                onChange={handleInputChange}
+                ref={register(formValidations.password)}
               />
+              <span className="form-error-message">
+                {errors?.password?.message}
+              </span>
             </div>
+
+            {requestError ? (
+              <span className="form-error-message form-group full-width">
+                {requestError}
+              </span>
+            ) : (
+              ""
+            )}
 
             <div className="form-group button-container">
               <button className="button button-right bg-secondary-color">
