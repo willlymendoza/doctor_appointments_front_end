0e16845d964f3a57cbd9e3a5fcdc9db7a0947ce3
Willian <wyllmh5@gmail.com>
2020-07-29 18:53:34 -0600

    Added validations to PatientsAdd page

--- src/components/PatientAddForm/index.js
+++ src/components/PatientAddForm/index.js
@@ -1,47 +1,82 @@
 import React from "react";
 import CardContainer from "../CardContainer";
 
-const PatientAddForm = ({ handleOnSubmit, handleInputChange }) => {
+const PatientAddForm = ({
+  onSubmitForm,
+  register,
+  handleSubmit,
+  errors,
+  requestError,
+}) => {
   return (
     <div className="form-container">
       <CardContainer title="Patient Info" color="secondary_color">
-        <form className="form" onSubmit={handleOnSubmit}>
+        <form className="form" onSubmit={handleSubmit(onSubmitForm)}>
           <div className="form-group">
             <label>First Name</label>
-            <input name="first_name" onChange={handleInputChange} />
+            <input name="first_name" ref={register} />
+            <span className="form-error-message">
+              {errors?.first_name?.message}
+            </span>
           </div>
+
           <div className="form-group">
             <label>Last Name</label>
-            <input name="last_name" onChange={handleInputChange} />
+            <input name="last_name" ref={register} />
+            <span className="form-error-message">
+              {errors?.last_name?.message}
+            </span>
           </div>
           <div className="form-group">
             <label>Personal Document ID</label>
-            <input name="personal_document_id" onChange={handleInputChange} />
+            <input name="personal_document_id" ref={register} />
+            <span className="form-error-message">
+              {errors?.personal_document_id?.message}
+            </span>
           </div>
           <div className="form-group">
             <label>Phone Number</label>
-            <input name="phone_number" onChange={handleInputChange} />
+            <input name="phone_number" ref={register} />
+            <span className="form-error-message">
+              {errors?.phone_number?.message}
+            </span>
           </div>
           <div className="form-group">
             <label>E-mail</label>
-            <input name="email" onChange={handleInputChange} />
+            <input name="email" ref={register} />
+            <span className="form-error-message">{errors?.email?.message}</span>
           </div>
           <div className="form-group">
             <label>City</label>
-            <input name="city" onChange={handleInputChange} />
+            <input name="city" ref={register} />
+            <span className="form-error-message">{errors?.city?.message}</span>
           </div>
           <div className="form-group">
             <label>Address</label>
-            <input name="address" onChange={handleInputChange} />
+            <input name="address" ref={register} />
+            <span className="form-error-message">
+              {errors?.address?.message}
+            </span>
           </div>
           <div className="form-group">
             <label>Age</label>
-            <input name="age" onChange={handleInputChange} />
+            <input name="age" ref={register} />
+            <span className="form-error-message">{errors?.age?.message}</span>
           </div>
           <div className="form-group">
-            <label>Sex</label>
-            <input name="sex" onChange={handleInputChange} />
+            <label>Sex (m, f)</label>
+            <input name="sex" ref={register} />
+            <span className="form-error-message">{errors?.sex?.message}</span>
           </div>
+
+          {requestError ? (
+            <span className="form-error-message form-group full-width">
+              {requestError}
+            </span>
+          ) : (
+            ""
+          )}
+
           <div className="form-group button-container">
             <button
               type="submit"
--- src/pages/Patients/AddPatient/index.js
+++ src/pages/Patients/AddPatient/index.js
@@ -4,39 +4,91 @@ import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import PatientAddForm from "../../../components/PatientAddForm";
+import { useForm } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers";
+import Joi from "@hapi/joi";
 
 const AddPatient = () => {
-  const formData = {
-    first_name: "",
-    last_name: "",
-    personal_document_id: "",
-    phone_number: "",
-    email: "",
-    city: "",
-    address: "",
-    age: "",
-    sex: "",
-  };
+  const schemaValidation = Joi.object({
+    first_name: Joi.string().trim().required().min(5).max(55).messages({
+      "string.empty": `"First Name" is required`,
+      "string.min": `"First Name" must be at least 5 characters long`,
+      "string.max": `"First Name" can only be up to 55 characters long`,
+    }),
+    last_name: Joi.string().trim().required().min(5).max(55).messages({
+      "string.empty": `"Last Name" is required`,
+      "string.min": `"Last Name" must be at least 5 characters long`,
+      "string.max": `"Last Name" can only be up to 55 characters long`,
+    }),
+    personal_document_id: Joi.string()
+      .trim()
+      .required()
+      .min(5)
+      .max(55)
+      .messages({
+        "string.empty": `"Personal Document ID" is required`,
+        "string.min": `"Personal Document ID" must be at least 5 characters long`,
+        "string.max": `"Personal Document ID" can only be up to 55 characters long`,
+      }),
+    phone_number: Joi.string().trim().required().min(8).max(25).messages({
+      "string.empty": `"Phone Number" is required`,
+      "string.min": `"Phone Number" must be at least 8 characters long`,
+      "string.max": `"Phone Number" can only be up to 25 characters long`,
+    }),
+    email: Joi.string()
+      .trim()
+      .email({ tlds: false })
+      .required()
+      .min(8)
+      .max(50)
+      .messages({
+        "string.empty": `"Email" is required`,
+        "string.email": `Please Provide a Valid Email address`,
+        "string.min": `"Email" must be at least 8 characters long`,
+        "string.max": `"Email" can only be up to 25 characters long`,
+      }),
+    city: Joi.string().trim().required().min(3).max(40).messages({
+      "string.empty": `"City" is required`,
+      "string.min": `"City" must be at least 3 characters long`,
+      "string.max": `"City" can only be up to 40 characters long`,
+    }),
+    address: Joi.string().trim().required().min(5).max(60).messages({
+      "string.empty": `"Address" is required`,
+      "string.min": `"Address" must be at least 3 characters long`,
+      "string.max": `"Address" can only be up to 40 characters long`,
+    }),
+    age: Joi.number().integer().required().min(1).max(150).messages({
+      "number.base": `"Age" must be a number`,
+      "number.empty": `"Age" is required`,
+      "number.unsafe": `"Age" must be a valid Integer number`,
+      "number.integer": `"Age" must be a valid Integer number`,
+      "number.min": `"Age" must be at least 1`,
+      "number.max": `That Age is Insane!! "Age" can only be up to 150`,
+    }),
+    sex: Joi.string().trim().required().min(1).max(1).valid("m", "f").messages({
+      "string.base": `"Sex" is requiredasdf`,
+      "string.empty": `"Sex" is required`,
+      "any.only": `"Sex" must be one of [m, f]`,
+      "string.min": `"Sex" must be at least 3 characters long`,
+      "string.max": `"Sex" can only be up to 40 characters long`,
+    }),
+  });
 
-  const [patientInfo, setPatientInfo] = useState(formData);
+  const [requestError, setRequestError] = useState(null);
+
   const userToken = useSelector((store) => store.authData.userToken);
+  const { register, handleSubmit, errors } = useForm({
+    resolver: joiResolver(schemaValidation),
+  });
   const history = useHistory();
 
-  const handleInputChange = (e) => {
-    setPatientInfo({
-      ...patientInfo,
-      [e.target.name]: e.target.value,
-    });
+  const onSubmitForm = (data) => {
+    axiosPostData(data);
   };
 
-  const handleOnSubmit = (e) => {
-    axiosPostData();
-    e.preventDefault();
-  };
-
-  const axiosPostData = async () => {
+  const axiosPostData = async (data) => {
     try {
-      await axios.post("http://localhost:5000/api/patients", patientInfo, {
+      await axios.post("http://localhost:5000/api/patients", data, {
         headers: {
           Authorization: userToken,
         },
@@ -44,7 +96,7 @@ const AddPatient = () => {
       history.push("/patients");
     } catch (error) {
       if (error.response) {
-        console.log(error.response.data);
+        setRequestError(error.response.data);
       }
     }
   };
@@ -53,8 +105,11 @@ const AddPatient = () => {
     <Fragment>
       <PageTitle title="ADD PATIENT" />
       <PatientAddForm
-        handleOnSubmit={handleOnSubmit}
-        handleInputChange={handleInputChange}
+        onSubmitForm={onSubmitForm}
+        register={register}
+        handleSubmit={handleSubmit}
+        errors={errors}
+        requestError={requestError}
       />
     </Fragment>
   );
