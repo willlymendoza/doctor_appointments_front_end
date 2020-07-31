6467cc7e75c45b5ae2fb93d7cf15f41c636b37b7
Willian <wyllmh5@gmail.com>
2020-07-31 16:14:27 -0600

    Moved functions from AppointmentsListTable

--- src/pages/Appointments/index.js
+++ src/pages/Appointments/index.js
@@ -1,12 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import PageTitle from "../../components/PageTitle";
 import AppointmentsListTable from "../../components/AppointmentsListTable";
+import { useSelector } from "react-redux";
+import useFetch from "../../hooks/useFetch";
 
 const Appointments = () => {
+  const [activePage, setActivePage] = useState(1);
+  const userToken = useSelector((store) => store.authData.userToken);
+
+  const appointmentsData = useFetch({
+    url: `/appointments?pageNumber=${activePage}&pageSize=5`,
+    userToken,
+  });
+
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+  };
   return (
     <Fragment>
-      <PageTitle title="APPOINTMENTS" />
-      <AppointmentsListTable />
+      {appointmentsData.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Fragment>
+          <PageTitle title="APPOINTMENTS" />
+          <AppointmentsListTable
+            appointmentsData={appointmentsData}
+            handlePageChange={handlePageChange}
+            activePage={activePage}
+          />
+        </Fragment>
+      )}
     </Fragment>
   );
 };
