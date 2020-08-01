b6e1f3c8f42340610bcf0ca040e2ca8d7e2f022a
Willian <wyllmh5@gmail.com>
2020-07-31 18:46:46 -0600

    Moved functions from PatientsListTable and used useFetch custom hook to get all the data from db

--- src/pages/Patients/index.js
+++ src/pages/Patients/index.js
@@ -1,12 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import PageTitle from "../../components/PageTitle";
 import PatientsListTable from "../../components/PatientsListTable";
+import { useSelector } from "react-redux";
+import useFetch from "../../hooks/useFetch";
 
 const Patients = () => {
+  const userToken = useSelector((store) => store.authData.userToken);
+  const [activePage, setActivePage] = useState(1);
+  const patientsData = useFetch({
+    url: `/patients?pageNumber=${activePage}&pageSize=5`,
+    userToken,
+  });
+
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+  };
+
   return (
     <Fragment>
-      <PageTitle title="PATIENTS" />
-      <PatientsListTable />
+      {patientsData.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Fragment>
+          <PageTitle title="PATIENTS" />
+          <PatientsListTable
+            patientsData={patientsData}
+            handlePageChange={handlePageChange}
+            activePage={activePage}
+          />
+        </Fragment>
+      )}
     </Fragment>
   );
 };
