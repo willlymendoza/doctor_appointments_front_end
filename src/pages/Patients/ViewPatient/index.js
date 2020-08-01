f4c9a8f8546d292fb8b4e6e6da85ef8bcd3954cc
Willian <wyllmh5@gmail.com>
2020-07-31 19:04:54 -0600

    Used useFetch custom hook & patientService to get and update data

--- src/pages/Patients/ViewPatient/index.js
+++ src/pages/Patients/ViewPatient/index.js
@@ -2,21 +2,27 @@ import React, { Fragment, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import PageTitle from "../../../components/PageTitle";
 import PatientViewForm from "../../../components/PatientViewForm";
-
-import "./styles.scss";
 import { useSelector } from "react-redux";
-import axios from "axios";
 import PatientAppointmentsList from "../PatientAppointmentsList";
 import { useForm } from "react-hook-form";
 import { joiResolver } from "@hookform/resolvers";
 import usePatientFormValidation from "../../../hooks/usePatientFormValidation";
+import useFetch from "../../../hooks/useFetch";
+import { update } from "../../../services/patientService";
+import "./styles.scss";
 
 const ViewPatient = () => {
   const { id } = useParams();
-  const [patientInfo, setPatientInfo] = useState([]);
-  const [appointmentsInfo, setAppointmentsInfo] = useState([]);
-  const [disabledInput, setDisabledInput] = useState(true);
   const userToken = useSelector((store) => store.authData.userToken);
+  const patientInfo = useFetch({
+    url: `/patients/${id}`,
+    userToken,
+  });
+  const appointmentsInfo = useFetch({
+    url: `/appointments/patient/${id}`,
+    userToken,
+  });
+  const [disabledInput, setDisabledInput] = useState(true);
   const formValidation = usePatientFormValidation();
 
   const { register, handleSubmit, errors } = useForm({
@@ -34,11 +40,7 @@ const ViewPatient = () => {
 
   const axiosPostData = async (data) => {
     try {
-      await axios.put(`http://localhost:5000/api/patients/${id}`, data, {
-        headers: {
-          Authorization: userToken,
-        },
-      });
+      await update(id, data, userToken);
       setDisabledInput(true);
     } catch (error) {
       if (error.response) {
@@ -47,62 +49,36 @@ const ViewPatient = () => {
     }
   };
 
-  useEffect(() => {
-    const getPatientInfo = async () => {
-      try {
-        const result = await axios.get(
-          `http://localhost:5000/api/patients/${id}`,
-          {
-            headers: {
-              Authorization: userToken,
-            },
-          }
-        );
-        setPatientInfo(result.data);
-      } catch (error) {
-        if (error.response) console.log(error.response.data);
-      }
-    };
-
-    getPatientInfo();
-  }, [id, userToken]);
-
-  useEffect(() => {
-    const getPatientAppointments = async () => {
-      try {
-        const result = await axios.get(
-          `http://localhost:5000/api/appointments/patient/${id}`,
-          {
-            headers: {
-              Authorization: userToken,
-            },
-          }
-        );
-        setAppointmentsInfo(result.data);
-      } catch (error) {
-        if (error.response) console.log(error.response.data);
-      }
-    };
-
-    getPatientAppointments();
-  }, [id, userToken]);
-
   return (
     <Fragment>
-      <PageTitle title={patientInfo.first_name + " " + patientInfo.last_name} />
-      <div className="patient-info-container">
-        <PatientViewForm
-          patientInfo={patientInfo}
-          onSubmitForm={onSubmitForm}
-          disabledInput={disabledInput}
-          handleEditClick={handleEditClick}
-          register={register}
-          handleSubmit={handleSubmit}
-          errors={errors}
-        />
+      {patientInfo.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Fragment>
+          <PageTitle
+            title={
+              patientInfo.response.first_name +
+              " " +
+              patientInfo.response.last_name
+            }
+          />
+          <div className="patient-info-container">
+            <PatientViewForm
+              patientInfo={patientInfo.response}
+              onSubmitForm={onSubmitForm}
+              disabledInput={disabledInput}
+              handleEditClick={handleEditClick}
+              register={register}
+              handleSubmit={handleSubmit}
+              errors={errors}
+            />
 
-        <PatientAppointmentsList appointmentsInfo={appointmentsInfo} />
-      </div>
+            <PatientAppointmentsList
+              appointmentsInfo={appointmentsInfo.response}
+            />
+          </div>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
