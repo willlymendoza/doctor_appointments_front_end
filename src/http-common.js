ddd7b117cb24e7ac064091d782dcd2789d3aca68
Willian <wyllmh5@gmail.com>
2020-08-01 16:30:56 -0600

    Edited baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function axiosConfig(token = "") {
   const config = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "http://192.168.50.96:5000/api",
     headers: {
       "Content-type": "application/json",
       Authorization: token,
