5fe28aef1e6526a385effdbc9423822dd24652f5
Willian <wyllmh5@gmail.com>
2020-08-01 00:09:52 -0600

    Added function to responsive menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import NavBar from "../NavBar";
 import Logo from "../Logo";
 import { useSelector } from "react-redux";
 
 import "./styles.scss";
+import BurgerButton from "../BurgerButton";
 
 const Header = () => {
   const isAuthenticated = useSelector(
     (store) => store.authData.isAuthenticated
   );
 
+  const [showMenu, setShowMenud] = useState(false);
+
+  const handleClick = () => {
+    setShowMenud(!showMenu);
+  };
+
   return (
     <Fragment>
       <div className="header-container"></div>
       <header className="header-content">
         <Logo />
-        {isAuthenticated ? <NavBar /> : ""}
+        {isAuthenticated ? (
+          <Fragment>
+            <NavBar handleClick={handleClick} showMenu={showMenu} />
+            <BurgerButton handleClick={handleClick} showMenu={showMenu} />
+          </Fragment>
+        ) : (
+          ""
+        )}
       </header>
     </Fragment>
   );
