d6b3a45f9316267a912b90f9fa18cbfcb76ec88c
Willian <wyllmh5@gmail.com>
2020-08-01 21:45:08 -0600

    Added propTypes validation

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import dateService from "../../services/dateService";
 
@@ -84,3 +85,9 @@ const Table = ({ labels, data, actions }) => {
 };
 
 export default Table;
+
+Table.propTypes = {
+  labels: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+};
