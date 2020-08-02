0f3695c61dd13cf2dfa505ffe663a20de65dfa8b
Willian <wyllmh5@gmail.com>
2020-08-01 21:41:46 -0600

    Added propTypes validation

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./styles.scss";
 
 const PageTitle = ({ title }) => {
@@ -11,3 +12,7 @@ const PageTitle = ({ title }) => {
 };
 
 export default PageTitle;
+
+PageTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+};
