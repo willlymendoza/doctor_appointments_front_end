ee8b078bd36fb48d79bf866f27c9837c467ea8bf
Willian <wyllmh5@gmail.com>
2020-08-02 16:24:47 -0600

    fixed not found data message

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -14,7 +14,9 @@ const Table = ({ labels, data, actions }) => {
         {actions ? <label>Actions</label> : ""}
       </div>
 
-      {data && <div className="table-grid full-width">No records found</div>}
+      {(!data || !data.length) && (
+        <div className="table-grid full-width">No records found</div>
+      )}
 
       {data.map((item) => (
         <div key={item._id} className="table-grid table-content">
