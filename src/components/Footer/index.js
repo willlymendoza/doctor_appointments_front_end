cb83ebecefde01f164e7a0eff8771428f47703ff
Willian <wyllmh5@gmail.com>
2020-08-16 20:06:38 -0600

    Added translations

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from "react";
+import { useTranslation } from "react-i18next";
 import "./styles.scss";
 
 function Footer() {
+  const { t } = useTranslation();
   return (
     <Fragment>
       <div className="footer-container"></div>
       <footer className="footer-content">
-        <h1>Created by Willian Mendoza</h1>
-        <h2>Software Engineer</h2>
+        <h1>{t("created_by.label")}</h1>
+        <h2>Full-Stack Developer</h2>
       </footer>
     </Fragment>
   );
