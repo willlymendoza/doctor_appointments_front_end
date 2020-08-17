10bf27d304d518bfb50b5564c423de95f9c44f86
Willian <wyllmh5@gmail.com>
2020-08-16 20:08:10 -0600

    Added translations

--- src/hooks/useAppointmentFormValidation.js
+++ src/hooks/useAppointmentFormValidation.js
@@ -1,24 +1,44 @@
 import Joi from "@hapi/joi";
+import { useTranslation } from "react-i18next";
 
-const useAppointmentFormValidation = () =>
-  Joi.object({
-    patient_id: Joi.string().required().messages({
-      "any.required": `"Patient" is required`,
-    }),
-    doctor_id: Joi.string().required().messages({
-      "any.required": `"Doctor" is required`,
-    }),
-    observations: Joi.string().trim().allow("").min(5).max(55).messages({
-      "string.min": `"Observations" must be at least 5 characters long`,
-      "string.max": `"Observations" can only be up to 55 characters long`,
-    }),
-    appointment_date: Joi.date().required().messages({
-      "date.base": `"Appointment Date" is required`,
-      "date.empty": `"Appointment Date" is required`,
-    }),
-    hour: Joi.string().trim().required().min(4).max(10).messages({
-      "string.empty": `"Hour" is required`,
-    }),
+const useAppointmentFormValidation = () => {
+  const { t } = useTranslation();
+
+  return Joi.object({
+    patient_id: Joi.string()
+      .required()
+      .messages({
+        "any.required": `${t("patient_required.label")}`,
+      }),
+    doctor_id: Joi.string()
+      .required()
+      .messages({
+        "any.required": `${t("doctor_required.label")}`,
+      }),
+    observations: Joi.string()
+      .trim()
+      .allow("")
+      .min(5)
+      .max(55)
+      .messages({
+        "string.min": `${t("observations_min.label")}`,
+        "string.max": `${t("observations_max.label")}`,
+      }),
+    appointment_date: Joi.date()
+      .required()
+      .messages({
+        "date.base": `${t("date_required.label")}`,
+        "date.empty": `${t("date_required.label")}`,
+      }),
+    hour: Joi.string()
+      .trim()
+      .required()
+      .min(4)
+      .max(10)
+      .messages({
+        "string.empty": `${t("hour_required.label")}`,
+      }),
   });
+};
 
 export default useAppointmentFormValidation;
