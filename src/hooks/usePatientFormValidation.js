87d65ff7e2fb85caf9ae72dcd5104a475ef99553
Willian <wyllmh5@gmail.com>
2020-08-16 20:08:34 -0600

    Added translations

--- src/hooks/usePatientFormValidation.js
+++ src/hooks/usePatientFormValidation.js
@@ -1,32 +1,50 @@
 import Joi from "@hapi/joi";
+import { useTranslation } from "react-i18next";
 
-const usePatientFormValidation = () =>
-  Joi.object({
-    first_name: Joi.string().trim().required().min(5).max(55).messages({
-      "string.empty": `"First Name" is required`,
-      "string.min": `"First Name" must be at least 5 characters long`,
-      "string.max": `"First Name" can only be up to 55 characters long`,
-    }),
-    last_name: Joi.string().trim().required().min(5).max(55).messages({
-      "string.empty": `"Last Name" is required`,
-      "string.min": `"Last Name" must be at least 5 characters long`,
-      "string.max": `"Last Name" can only be up to 55 characters long`,
-    }),
+const usePatientFormValidation = () => {
+  const { t } = useTranslation();
+
+  return Joi.object({
+    first_name: Joi.string()
+      .trim()
+      .required()
+      .min(5)
+      .max(55)
+      .messages({
+        "string.empty": `${t("first_name_required.label")}`,
+        "string.min": `${t("first_name_min.label")}`,
+        "string.max": `${t("first_name_max.label")}`,
+      }),
+    last_name: Joi.string()
+      .trim()
+      .required()
+      .min(5)
+      .max(55)
+      .messages({
+        "string.empty": `${t("last_name_required.label")}`,
+        "string.min": `${t("last_name_min.label")}`,
+        "string.max": `${t("last_name_max.label")}`,
+      }),
     personal_document_id: Joi.string()
       .trim()
       .required()
       .min(5)
       .max(55)
       .messages({
-        "string.empty": `"Personal Document ID" is required`,
-        "string.min": `"Personal Document ID" must be at least 5 characters long`,
-        "string.max": `"Personal Document ID" can only be up to 55 characters long`,
+        "string.empty": `${t("personal_id_required.label")}`,
+        "string.min": `${t("personal_id_min.label")}`,
+        "string.max": `${t("personal_id_max.label")}`,
       }),
-    phone_number: Joi.string().trim().required().min(8).max(25).messages({
-      "string.empty": `"Phone Number" is required`,
-      "string.min": `"Phone Number" must be at least 8 characters long`,
-      "string.max": `"Phone Number" can only be up to 25 characters long`,
-    }),
+    phone_number: Joi.string()
+      .trim()
+      .required()
+      .min(8)
+      .max(25)
+      .messages({
+        "string.empty": `${t("phone_number_required.label")}`,
+        "string.min": `${t("phone_number_min.label")}`,
+        "string.max": `${t("phone_number_max.label")}`,
+      }),
     email: Joi.string()
       .trim()
       .email({ tlds: false })
@@ -34,36 +52,58 @@ const usePatientFormValidation = () =>
       .min(8)
       .max(50)
       .messages({
-        "string.empty": `"Email" is required`,
-        "string.email": `Please Provide a Valid Email address`,
-        "string.min": `"Email" must be at least 8 characters long`,
-        "string.max": `"Email" can only be up to 25 characters long`,
+        "string.empty": `${t("email_required.label")}`,
+        "string.email": `${t("email_valid.label")}`,
+        "string.min": `${t("email_min.label")}`,
+        "string.max": `${t("email_max.label")}`,
       }),
-    city: Joi.string().trim().required().min(3).max(40).messages({
-      "string.empty": `"City" is required`,
-      "string.min": `"City" must be at least 3 characters long`,
-      "string.max": `"City" can only be up to 40 characters long`,
-    }),
-    address: Joi.string().trim().required().min(5).max(60).messages({
-      "string.empty": `"Address" is required`,
-      "string.min": `"Address" must be at least 3 characters long`,
-      "string.max": `"Address" can only be up to 40 characters long`,
-    }),
-    age: Joi.number().integer().required().min(1).max(150).messages({
-      "number.base": `"Age" must be a number`,
-      "number.empty": `"Age" is required`,
-      "number.unsafe": `"Age" must be a valid Integer number`,
-      "number.integer": `"Age" must be a valid Integer number`,
-      "number.min": `"Age" must be at least 1`,
-      "number.max": `That Age is Insane!! "Age" can only be up to 150`,
-    }),
-    sex: Joi.string().trim().required().min(1).max(1).valid("m", "f").messages({
-      "string.base": `"Sex" is requiredasdf`,
-      "string.empty": `"Sex" is required`,
-      "any.only": `"Sex" must be one of [m, f]`,
-      "string.min": `"Sex" must be at least 3 characters long`,
-      "string.max": `"Sex" can only be up to 40 characters long`,
-    }),
+    city: Joi.string()
+      .trim()
+      .required()
+      .min(3)
+      .max(40)
+      .messages({
+        "string.empty": `${t("city_required.label")}`,
+        "string.min": `${t("city_min.label")}`,
+        "string.max": `${t("city_max.label")}`,
+      }),
+    address: Joi.string()
+      .trim()
+      .required()
+      .min(5)
+      .max(60)
+      .messages({
+        "string.empty": `${t("address_required.label")}`,
+        "string.min": `${t("address_min.label")}`,
+        "string.max": `${t("address_max.label")}`,
+      }),
+    age: Joi.number()
+      .integer()
+      .required()
+      .min(1)
+      .max(150)
+      .messages({
+        "number.base": `${t("age_number.label")}`,
+        "number.empty": `${t("age_required.label")}`,
+        "number.unsafe": `${t("age_valid.label")}`,
+        "number.integer": `${t("age_valid.label")}`,
+        "number.min": `${t("age_min.label")}`,
+        "number.max": `${t("age_max.label")}`,
+      }),
+    sex: Joi.string()
+      .trim()
+      .required()
+      .min(1)
+      .max(1)
+      .valid("m", "f")
+      .messages({
+        "string.base": `${t("sex_required.label")}`,
+        "string.empty": `${t("sex_required.label")}`,
+        "any.only": `${t("sex_mf.label")}`,
+        "string.min": `${t("sex_min.label")}`,
+        "string.max": `${t("sex_max.label")}`,
+      }),
   });
+};
 
 export default usePatientFormValidation;
